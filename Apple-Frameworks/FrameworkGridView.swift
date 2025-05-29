378c5f11d0d7b9f68fc544c57dc2dfa2e2b39723
VRM <vromanm@mail.ru>
2025-05-29 15:48:02 +0300

    создал FrameworkTitleView

--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -15,37 +15,39 @@ struct FrameworkGridView: View {
     var frameworks = MockData.frameworks
     
     var body: some View {
-        ZStack {
+        NavigationView {
             ScrollView {
                 LazyVGrid(columns: columns) {
-                    ForEach(frameworks.indices, id: \.self) { index in
-                        FrameworkTitleView(name: frameworks[index].name, imageName: frameworks[index].imageName)
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
                     }
                 }
             }
+            .navigationTitle("Frameworks")
         }
     }
 }
 
 struct FrameworkTitleView: View {
-    let name: String
-    let imageName: String
+    let framework: Framework
     
     var body: some View {
         VStack {
-            Image(imageName)
+            Image(framework.imageName)
                 .resizable()
                 .frame(width: 90, height: 90, alignment: .center)
                 .aspectRatio(contentMode: .fit)
-            Text(name)
+            Text(framework.name)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit()
                 .minimumScaleFactor(0.6)
         }
+        .padding()
     }
 }
 
 #Preview {
     FrameworkGridView()
+        .preferredColorScheme(.dark)
 }
